230d6e878ff49233cdeba8d62ea16a3fa1844617
Zethra71 <120029044+Zethra71@users.noreply.github.com>
2023-09-04 15:53:55 +0200

    Added all paragraphs and character

--- challenge_day1/liss_name.py
+++ challenge_day1/liss_name.py
@@ -2,13 +2,13 @@ def myname():
     return "Liss"
 
 def para3():
-    return ""
+    return "One day, the master overlords of Earth-2 decided to give a test to their loyal although intellectually-challenged pupils. Specifically, they asked the population of Earth-2 to master the dark arts of Git, an ancient mechanism whereby, through a distributed graph theory tree model, programming projects can be swiftly organised and coordinated. They felt that, with this, they could finally drive GitOrc into submission. The Great Sorcerers, given that they were children of Garlok the destroyer, were the bravest in the land, and so decided to take up the challenge. They also asked help from the Cloaked Ones, though they were unable to help. This kept Calysass in a complcitaed position. "
 
 def para9():
-    return ""
+    return "Desperate to save the other sorcerers and left with no other option, Calysass tried to telepathically connect with the GitOrc’s central wisdom source - his heart. They closed their eyes and focused hard, imagining what Garlok the Mighty himself would do. Without the support of the Cloaked Ones and with threats from the ancient Overlords looming in the foggy air of Earth-2, options were limited. But Calysass would not surronder the battle of improving the Second Earth. "
 
 def para15():
-    return ""
+    return "With time, hearts softened, grudges were forgiven, and a newfound understanding bloomed like the blossoms in spring. The world, once again, embraced its inhabitants with open arms, a place where happiness and welcome extended to all who chose to share in its beauty. Everyone, including Calysass, Garlok, the Cloaked Ones, the GitOrc, and Garlok were left with things to contemplate. Their behaviour, their perspectives, their crimes. "
 
 def triske():
     return "Calysass Triske"

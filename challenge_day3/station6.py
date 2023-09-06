6a8a9670be796bc9d72630d756cc701fa3d4e478
Zethra71 <120029044+Zethra71@users.noreply.github.com>
2023-09-06 12:55:32 +0200

    pls

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -1,19 +1,14 @@
 import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+import math
 import matplotlib.pyplot as plt
 
 x = [0.6, 0.8, 1.1, 1.5, 1.9, 2.9]
 y = [0.5646, 0.7174, 0.8912, 0.9975, 0.9463, 0.2392]
 
-poly = PolynomialFeatures(degree=2, include_bias=False).fit_transform(np.array(x).reshape(-1, 1))
 
-model = LinearRegression().fit(poly, y)
-y_pred = model.predict(poly)
-
-plt.plot(x, y)
-plt.plot(x, y_pred)
 
 def solution_station_6(x):
-    return model.coef_[1] * x ** 2 + model.coef_[1] * x + model.intercept_
+    return math.sin(x)
+
+
 

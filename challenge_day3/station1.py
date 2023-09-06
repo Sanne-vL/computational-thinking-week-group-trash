27bc8a934a9a6a379d8d134d32e3391f8e0bef30
HielkeT <126685281+HielkeT@users.noreply.github.com>
2023-09-06 11:12:00 +0200

    function solution to station 1

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,3 +1,22 @@
-# hint: italian mathematician
+def solution_station_1(n):
+    
+    first = 0
+    second = 1
+    count = 0
 
-def solution_station_1
+    if n <= 0:
+        return first
+    elif n == 1:
+        return second
+    else:
+        while count < n - 1:
+            nth = first + second
+            first =  second
+            second = nth
+            count += 1
+
+        return second
+
+n = int(input())
+result = solution_station_1(n)
+print(f'The {n}-th Fibonacci number is {result}')

455665d003ada3e14db2b27de22b6a6771b4e660
eunjeongc <ejchoi0722@gmail.com>
2023-09-06 14:03:31 +0200

    edited

---
+++ challenge_day3/.ipynb_checkpoints/station2-checkpoint.py
@@ -0,0 +1,21 @@
+import datetime
+
+def get_day_of_week(date_str):
+ 
+    weekday_names = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日']
+
+    date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+
+    day_of_week = date.weekday()
+
+    day_name = weekday_names[day_of_week]
+
+    return day_name
+   
+input_date = input()
+day = get_day_of_week(input_date)
+print(f"{day}")
+
+
+
+

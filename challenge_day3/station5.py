bd3ab648f7433b09bae41434a25a8d05b25340be
Sanne-vL <sanne.van.loo@student.uva.nl>
2023-09-06 15:04:30 +0200

    Fixed data issue

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-dic = {1 : ['Mika', 'Maria', 'Isis', 'Rosa', 'Maja', 'Damaris'], 2 : ['Emily ', 'Celia', 'Carine', 'Sabrina', 'Maria', 'Gur'], 3 : ['Elisa', 'Sari', 'Dave', 'Dima', 'Jesper', 'Martyna'], 4 : ['Kelt', 'Sebastian', 'Quanpu', 'Ruben', 'Sofia', 'Gabriella'], 5 : ['Kian', 'Sahir', 'Tom', 'Gonzalo', 'Ameer', 'Teun'], 6 : ['Angelica', 'Matas', 'Caleb', 'Angeline', 'Raven', 'Paulina'], 7 : ['Martyna', 'Maja ', 'Mate', 'Vincent', 'Eryk', 'Emma'], 8 : ['Hielke', 'Liss', 'Beatris', 'Caio', 'Sally', 'Sanne'], 9 : ['Atlas', 'Elli', 'Felix', 'Diana', 'Yash'], 10 : ['Akanksha', 'Charlie', 'Andrey', 'Max', 'Hugo', 'Al-Fatihi'], 11 : ['Misha', 'Ioanna', 'Ella', 'Cristian', 'Vanessa'], 12 : ['Luca', 'Rachna', 'Jelle', 'Karolina', 'Yuyue', 'Alexia']}
+dic = {1 : ['Mika', 'Maria', 'Isis', 'Rosa', 'Maja', 'Damaris'], 2 : ['Emily ', 'Celia', 'Carine', 'Sabrina', 'Maria', 'Gur'], 3 : ['Elisa', 'Sari', 'Kavin', 'Dima', 'Jesper', 'Martyna'], 4 : ['Kelt', 'Sebastian', 'Quanpu', 'Ruben', 'Sofia', 'Gabriella'], 5 : ['Kian', 'Sahir', 'Tom', 'Gonzalo', 'Ameer', 'Teun'], 6 : ['Angelica', 'Matas', 'Caleb', 'Angeline', 'Raven', 'Paulina'], 7 : ['Martyna', 'Maja ', 'Mate', 'Vincent', 'Eryk', 'Emma'], 8 : ['Hielke', 'Liss', 'Beatris', 'Caio', 'Sally', 'Sanne'], 9 : ['Atlas', 'Elli', 'Felix', 'Diana', 'Yash'], 10 : ['Akanksha', 'Charlie', 'Andrey', 'Max', 'Hugo', 'Al-Fatihi'], 11 : ['Misha', 'Ioanna', 'Ella', 'Cristian', 'Vanessa'], 12 : ['Luca', 'Rachna', 'Jelle', 'Karolina', 'Yuyue', 'Alexia']}
 
 def solution_station_5(name):
     for i in range(1, 13):

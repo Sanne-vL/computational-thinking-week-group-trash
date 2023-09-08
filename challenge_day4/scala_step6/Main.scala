70d362563386624b40433cddbc329a67420a9b47
Zethra71 <120029044+Zethra71@users.noreply.github.com>
2023-09-08 14:44:35 +0200

    Liss

--- challenge_day4/scala_step6/Main.scala
+++ challenge_day4/scala_step6/Main.scala
@@ -3,8 +3,8 @@ import scala.util.Try
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val filePath = "data6.txt"
-    val lines = scala.io.Source.fromFile(/Users/liss/Desktop/TRASHGIT/computational-thinking-week-group-trash/challenge_day4/fulldata/data6.txt).getLines().toList
+    val filePath = "/Users/liss/Desktop/TRASHGIT/computational-thinking-week-group-trash/challenge_day4/fulldata/data6.txt"
+    val lines = scala.io.Source.fromFile("data6.txt").getLines().toList
     val outputLines = lines.zipWithIndex.map {
       case (line, 0) => s"$line,Comments"
       case (line, _) =>
@@ -23,6 +23,6 @@ object Main {
         }
     }
 
-    Files.write(Paths.get("data7.txt"), outputLines.mkString("\n").getBytes)
+    Files.write(Paths.get("/Users/liss/Desktop/TRASHGIT/computational-thinking-week-group-trash/challenge_day4/fulldata/data7.txt"), outputLines.mkString("\n").getBytes)
   }
 }
